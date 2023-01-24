50f3cb68ce214655bd947957fd29aa58c76dcb9f
Rana Ozkan <hranaozkan@gmail.com>
2023-01-24 12:36:07 +0300

    mySQL notlari

---
+++ day03_select_regexp_like.sql
@@ -0,0 +1,40 @@
+/*==================================SELECT-REGEXP_LIKE=============================================
+ Daha karmasik pattern ile sorgulama islemi icin REGEXP_LIKE kullanilabilir.
+  -- 'c' => case-sensitive demektir
+  -- 'i' => case-insensitive demektir ve default case-insensitive aktiftir.
+  
+-----Syntax-----
+ SELECT sutun1,sutun2,...
+ FROM tablo_adi
+ WHERE REGEXP_LIKE(sutun_adi, 'pattern[]','c'[])
+=========================================================================================*/
+
+USE sys;
+
+
+CREATE TABLE kelimeler
+    (
+        id int UNIQUE,
+        kelime VARCHAR(50) NOT NULL,
+        harf_sayisi int
+    );
+    
+INSERT INTO kelimeler VALUES (1001, 'hot', 3);
+INSERT INTO kelimeler VALUES (1002, 'hat', 3);
+INSERT INTO kelimeler VALUES (1003, 'hit', 3);
+INSERT INTO kelimeler VALUES (1004, 'hbt', 3);
+INSERT INTO kelimeler VALUES (1005, 'hct', 3);
+INSERT INTO kelimeler VALUES (1006, 'adem', 4);
+INSERT INTO kelimeler VALUES (1007, 'selim', 5);
+INSERT INTO kelimeler VALUES (1008, 'yusuf', 5);
+INSERT INTO kelimeler VALUES (1009, 'hip', 3);
+INSERT INTO kelimeler VALUES (1010, 'HOT', 3);
+INSERT INTO kelimeler VALUES (1011, 'hOt', 3);
+INSERT INTO kelimeler VALUES (1012, 'h9t', 3);
+INSERT INTO kelimeler VALUES (1013, 'hoot', 4);
+INSERT INTO kelimeler VALUES (1014, 'haaat', 5);
+INSERT INTO kelimeler VALUES (1015, 'hooooot', 7);
+
+ SELECT * FROM kelimeler;
+ 
+
